729788775d0481505c680afe443ccb4d784300a4
Smudger1 <mugoftea37@gmail.com>
2025-07-28 20:34:07 +0100

    start of calib script

--- testScript.py
+++ testScript.py
@@ -5,5 +5,5 @@ import os
 cam = Camera()
 
 cam.start_preview()
-cam.take_photo(f"./testImages/new_image.jpg", interval=2) #save the image to your desktop
+cam.take_photo(f"./testImages/new_image.jpg") #save the image to your desktop
 cam.stop_preview()
