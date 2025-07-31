60c48038b71683d271916ec49c296cb34d6bc19b
Smudger1 <mugoftea37@gmail.com>
2025-07-31 16:03:34 +0100

    Update testScript.py

--- testScript.py
+++ testScript.py
@@ -4,7 +4,7 @@ import os
 
 cam = Camera()
 
-cam.start_preview()
-cam.take_photo(f"./testImages/setupImg.jpg") #save the image to your desktop
-cam.stop_preview()
+##cam.start_preview()
+cam.take_photo(f"setupImg.jpg") #save the image to your desktop
+##cam.stop_preview()
 
