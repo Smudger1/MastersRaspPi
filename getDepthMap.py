d9270f957fc118228c1a516c4325043235b35c33
Smudger1 <mugoftea37@gmail.com>
2025-08-06 15:01:26 +0100

    Update getDepthMap.py

--- getDepthMap.py
+++ getDepthMap.py
@@ -17,15 +17,18 @@ def getDepthMap(frame):
     DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
     print(f"Using device: {DEVICE}")
 
-    checkpoint_path = f'./Depth-Anything-V2/checkpoints/depth_anything_v2_metric_{dataset}_{encoder}.pth'
-    if not os.path.exists(checkpoint_path):
-        print(f"ERROR: Checkpoint file not found at {checkpoint_path}")
-        return None
+    
 
     encoder = 'vits' # or 'vits', 'vitb'
     dataset = 'hypersim' # 'hypersim' for indoor model, 'vkitti' for outdoor model
     #max_depth = 20 # 20 for indoor model, 80 for outdoor model
 
+
+    checkpoint_path = f'./Depth-Anything-V2/checkpoints/depth_anything_v2_metric_{dataset}_{encoder}.pth'
+    if not os.path.exists(checkpoint_path):
+        print(f"ERROR: Checkpoint file not found at {checkpoint_path}")
+        return None
+        
     print(f"Loading DepthAnythingV2 model with encoder: {encoder}, dataset: {dataset}")
     model = DepthAnythingV2(**model_configs[encoder])
     model.load_state_dict(torch.load(f'./Depth-Anything-V2/checkpoints/depth_anything_v2_metric_{dataset}_{encoder}.pth', map_location='cpu'))
