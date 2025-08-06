4b8a93bf713e1a29c80ed708049c289cca8515f7
Smudger1 <mugoftea37@gmail.com>
2025-08-06 14:59:37 +0100

    Update getDepthMap.py

--- getDepthMap.py
+++ getDepthMap.py
@@ -17,6 +17,11 @@ def getDepthMap(frame):
     DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
     print(f"Using device: {DEVICE}")
 
+    checkpoint_path = f'./Depth-Anything-V2/checkpoints/depth_anything_v2_metric_{dataset}_{encoder}.pth'
+    if not os.path.exists(checkpoint_path):
+        print(f"ERROR: Checkpoint file not found at {checkpoint_path}")
+        return None
+
     encoder = 'vits' # or 'vits', 'vitb'
     dataset = 'hypersim' # 'hypersim' for indoor model, 'vkitti' for outdoor model
     #max_depth = 20 # 20 for indoor model, 80 for outdoor model
@@ -30,8 +35,11 @@ def getDepthMap(frame):
     print("Processing image for depth estimation...")
     image = cv.imread('./objectDetectionImgs/frame1.jpg')  # Load the image
 
+    image_rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    print(f"Image shape: {image_rgb.shape}, dtype: {image_rgb.dtype}")
+    
     print(f"Image shape: {image.shape}, dtype: {image.dtype}")
-    depth = model.infer_image(image) # HxW depth map in meters in numpy
+    depth = model.infer_image(image_rgb) # HxW depth map in meters in numpy
 
     print(f"Depth map shape: {depth.shape}, dtype: {depth.dtype}")
 
